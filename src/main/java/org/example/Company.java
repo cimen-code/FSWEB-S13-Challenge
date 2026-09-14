dfda869082f938c1a3e4be8a0d046e1c9a589e25
cimen-code <hakancimen92@gmail.com>
2026-09-14 19:44:16 +0300

    Complete FSWEB S13 Challenge

---
+++ src/main/java/org/example/Company.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Company {
+
+    private int id;
+    private String name;
+    private double giro;
+    private String[] developerNames;
+
+    public Company(int id, String name, double giro, String[] developerNames) {
+        this.id = id;
+        this.name = name;
+        this.giro = Math.max(0, giro);
+        this.developerNames = developerNames;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGiro() {
+        return giro;
+    }
+
+    public void setGiro(double giro) {
+        this.giro = Math.max(0, giro);
+    }
+
+    public String[] getDeveloperNames() {
+        return developerNames;
+    }
+
+    public void setDeveloperNames(String[] developerNames) {
+        this.developerNames = developerNames;
+    }
+
+    public void addEmployee(int index, String name) {
+        if (index < 0 || index >= developerNames.length) {
+            System.out.println("Geçersiz index.");
+            return;
+        }
+
+        if (developerNames[index] != null) {
+            System.out.println("Bu index zaten dolu.");
+            return;
+        }
+
+        developerNames[index] = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", giro=" + giro +
+                ", developerNames=" + Arrays.toString(developerNames) +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
-
+import org.example.enums.Plan;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    runApp();
+}
+    public static void runApp() {
+    Healthplan healthplan = new Healthplan(1, "A Sigorta", Plan.BASIC);
+
+    String[] healthPlans = new String[2];
+    healthPlans[0] = healthplan.getName();
+
+    Employee employee = new Employee(
+            1,
+            "John Doe",
+            "jd@test.com",
+            "1234",
+            healthPlans
+    );
+
+    String[] developerNames = new String[2];
+    developerNames[0] = employee.getFullName();
+
+    Company company = new Company(
+            1,
+            "Workintech",
+            1000,
+            developerNames
+    );
+
+    System.out.println(healthplan);
+    System.out.println(employee);
+    System.out.println(company);
+}
 }
